c6113b090bac1497687bd7e6d2ab50afcd01fd5c
Ethan Jacoby <ejacoby2099@gmail.com>
2025-11-27 10:19:04 +0200

    Add files via upload

---
+++ Project 2/Drone_Racing_Sim/Drone.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drone_Racing_Sim
+{
+    //Defining the class of the drone to be used in other functions
+    public class Drone
+    {
+        public string Name { get; set; }
+        public int DistanceTraveled { get; set; }
+        public int Speed { get; set; } // Distance covered per iteration
+
+        public Drone(string name, int speed)
+        {
+            Name = name;
+            DistanceTraveled = 0;
+            Speed = speed;
+        }
+
+        public void Advance()
+        {
+            DistanceTraveled += Speed;
+        }
+    }
+
+    public class Race
+    {
+        private int _trackLength;
+        private List<Drone> _racers;
+
+        public Race(int trackLength, List<Drone> racers)
+        {
+            _trackLength = trackLength;
+            _racers = racers;
+        }
+
+        
+
+        //Win function
+        private void FindWinner()
+        {
+            var winner = _racers.OrderByDescending(r => r.DistanceTraveled).First();
+            Console.WriteLine($"\n{winner.Name} wins the race!");
+        }
+
+        //Run function
+        public void Run()
+        {
+            Console.WriteLine("Race begin... Drones must travel 25km!");
+            int iteration = 0;
+
+            while (!_racers.All(r => r.DistanceTraveled >= _trackLength))
+            {
+                iteration++;
+                Console.WriteLine($"\nIteration {iteration}:");
+
+                foreach (var racer in _racers)
+                {
+                    // Only advance racers who haven't finished yet
+                    if (racer.DistanceTraveled < _trackLength)
+                    {
+                        racer.Advance();
+                    }
+                    Console.WriteLine($"{racer.Name}: {racer.DistanceTraveled} km");
+                }
+
+                Console.WriteLine("Press any key to advance to the next iteration.");
+                Console.ReadKey();
+                Console.Clear();
+            }
+
+            Console.WriteLine("\nRace complete!");
+            FindWinner();
+
+        }
+    }
+}
---
+++ Project 2/Drone_Racing_Sim/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Drone_Racing_Sim
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the drone racing simulation!");
+
+            //Random distance that the drone travel
+            Random random = new Random();
+
+            int num1 = random.Next(1, 7); // 
+            int num2 = random.Next(1, 7); //
+            int num3 = random.Next(1, 7); //  Generates a random number between 1 and 6 
+            int num4 = random.Next(1, 7); //
+            int num5 = random.Next(1, 7); //
+
+            // Drones
+            var racers = new List<Drone>
+            {
+                new Drone("Drone 1", num1),
+                new Drone("Drone 2", num2),
+                new Drone("Drone 3", num3),
+                new Drone("Drone 4", num4),
+                new Drone("Drone 5", num5)
+            };
+
+            // Define the distance
+            int distance = 25;
+
+            // Create and run the race
+            Race race = new Race(distance, racers);
+            race.Run();
+
+            Console.WriteLine("\nPress any key to exit.");
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
